811e35af7f0f46d9c53c82dc1789f1b7897f8fa0
JerichoBarquez <66514653+JerichoBarquez@users.noreply.github.com>
2020-06-06 18:12:51 +0800

    Add files via upload

---
+++ ShoppingBasket/src/main/java/com/project/shopping/entity/Basket.java
@@ -0,0 +1,30 @@
+package com.project.shopping.entity;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Transient;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+public class Basket {
+	
+	@Id @GeneratedValue @Getter @Setter
+	private int basket_id;
+	@Getter @Setter
+	private String basket_name;
+	
+	@Getter @Setter
+	@OneToMany(mappedBy="basket")
+	private List<Product> list_prod;
+	
+	@Getter @Setter
+	@Transient
+	private Double tot;
+
+}
